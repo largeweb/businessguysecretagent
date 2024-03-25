ff34bb3fa46545728663e5186b397ab402f53f4e
matt <matthewgordon64@gmail.com>
2024-03-24 21:19:11 -0400

    metadata name change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "MyStory",
-  description: "MyStory is a platform for experiences.",
+  title: "Secret Agent",
+  description: "Business Guy Secret Agent",
 };
 
 export default function RootLayout({
